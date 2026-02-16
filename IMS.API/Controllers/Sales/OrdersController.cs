bc6633f859e4baaefdbce18f4aa1f66385fb2bfb
Ahmed Esam <buhussien2@gmail.com>
2026-02-16 12:39:12 +0200

    add-order-controller

---
+++ IMS.API/Controllers/Sales/OrdersController.cs
@@ -0,0 +1,75 @@
+using IMS.Application.Common.DTOs;
+using IMS.Application.Modules.Sales.DTOs.Order;
+using IMS.Application.Modules.Sales.DTOs.Order.OrderItem;
+using IMS.Application.Modules.Sales.Filters;
+using IMS.Application.Modules.Sales.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IMS.API.Controllers.Sales;
+
+[Route("api/sales/[controller]")]
+[ApiController]
+public class OrdersController : ControllerBase
+{
+    private readonly ISalesOrderService _salesOrderService;
+    public OrdersController(ISalesOrderService salesOrderService)
+    {
+        
+        _salesOrderService = salesOrderService;
+    }
+
+    [HttpPost("create-draft-order")]
+    public async Task<ApiResponse<OrderCreatedResult>> CreateDraftOrder(CreateOrderDto dto)
+    {
+        return new ApiResponse<OrderCreatedResult>(await _salesOrderService.CreateDraftOrder(dto));
+    }
+
+    [HttpPost("add-item")]
+    public async Task<ApiResponse<OrderItemDto>> AddItem(AddItemDto dto)
+    {
+        return new ApiResponse<OrderItemDto>(await _salesOrderService.AddItem(dto));
+    }
+
+    [HttpPost("confirm/{orderId}")]
+    public async Task<ApiResponse<bool>> Confirm(Guid orderId)
+    {
+        return new ApiResponse<bool>(await _salesOrderService.Confirm(orderId));
+    }
+
+    [HttpPost("cancel/{orderId}")]
+    public async Task<ApiResponse<bool>> Cancel(Guid orderId)
+    {
+        return new ApiResponse<bool>(await _salesOrderService.Cancel(orderId));
+    }
+
+    [HttpPost("complete/{orderId}")]
+    public async Task<ApiResponse<bool>> Complete(Guid orderId)
+    {
+        return new ApiResponse<bool>(await _salesOrderService.Complete(orderId));
+    }
+    
+    [HttpPost("remove-item/{orderId}/{itemId}")]
+    public async Task<ApiResponse<bool>> RemoveItem(Guid orderId, Guid itemId)
+    {
+        return new ApiResponse<bool>(await _salesOrderService.RemoveItem(orderId, itemId));
+    }
+    [HttpGet("{orderId}")]
+    public async Task<ApiResponse<OrderDetailsDto>> GetOne(Guid orderId)
+    {
+        return new ApiResponse<OrderDetailsDto>(await _salesOrderService.GetOrderDetails(orderId));
+    }
+
+    [HttpGet("get-orders")]
+    public async Task<PaginatedApiResponse<OrderDto>> GetOrders([FromQuery]OrderFilter filter)
+    {
+        return await _salesOrderService.GetAll(filter);
+    }
+
+    [HttpPatch("update-item-quantity/{orderId}/{itemId}/{quantity}")]
+    public async Task<ApiResponse<bool>> UpdateItemQuantity(Guid orderId, Guid itemId, int quantity)
+    {
+        return new ApiResponse<bool>(await _salesOrderService.UpdateItemQuantity(orderId, itemId, quantity));
+    }
+    
+
+}
